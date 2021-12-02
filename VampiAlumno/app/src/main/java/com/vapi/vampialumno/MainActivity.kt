1614369cb98d1d1fe716ee85c70934e6b0ce8a2d
PacoSalazarR <fran.pacorro2001@gmail.com>
2021-12-01 18:49:09 -0600

    Creación proyecto Alumno

---
+++ VampiAlumno/app/src/main/java/com/vapi/vampialumno/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vapi.vampialumno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
