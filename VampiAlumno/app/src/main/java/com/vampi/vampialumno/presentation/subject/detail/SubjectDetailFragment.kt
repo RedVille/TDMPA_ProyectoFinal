a6a6fdf2448b0fdbf454dddf0ed489c719511c5c
PacoSalazarR <fran.pacorro2001@gmail.com>
2021-12-08 16:25:49 -0600

    VampiAlumno -  Detalle Materia

--- VampiAlumno/app/src/main/java/com/vampi/vampialumno/presentation/subject/detail/SubjectDetailFragment.kt
+++ VampiAlumno/app/src/main/java/com/vampi/vampialumno/presentation/subject/detail/SubjectDetailFragment.kt
@@ -6,27 +6,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.vampi.vampialumno.R
+import com.vampi.vampialumno.core.extension.failure
+import com.vampi.vampialumno.core.extension.loadFromURLCircular
+import com.vampi.vampialumno.core.extension.loadFromURLRounded
+import com.vampi.vampialumno.core.extension.observe
+import com.vampi.vampialumno.core.presentation.BaseFragment
+import com.vampi.vampialumno.databinding.SubjectDetailFragmentBinding
+import com.vampi.vampialumno.databinding.SubjectsFragmentBinding
+import com.vampi.vampialumno.domain.model.DetalleAlumno
+import com.vampi.vampialumno.domain.model.Usuario
+import com.vampi.vampialumno.presentation.profile.detail.ProfileDetailFragment
+import com.vampi.vampialumno.presentation.profile.edit.EditProfileFragmentArgs
+import com.vampi.vampialumno.presentation.subject.list.SubjectsFragment
+import com.vampi.vampialumno.presentation.subject.list.SubjectsFragment.Companion.subjectPicture
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.WithFragmentBindings
 
-class SubjectDetailFragment : Fragment() {
+@AndroidEntryPoint
+@WithFragmentBindings
+class SubjectDetailFragment : BaseFragment(R.layout.subject_detail_fragment) {
 
-    companion object {
-        fun newInstance() = SubjectDetailFragment()
+    private lateinit var binding: SubjectDetailFragmentBinding
+    private val args: SubjectDetailFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    private lateinit var viewModel: SubjectDetailViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.subject_detail_fragment, container, false)
+    override fun setBinding(view: View) {
+        binding = SubjectDetailFragmentBinding.bind(view)
+        printSubject(args.subject)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SubjectDetailViewModel::class.java)
-        // TODO: Use the ViewModel
+    fun printSubject (detalleAlumno: DetalleAlumno) {
+        binding.txvDetailSubjectTitle.text = detalleAlumno.nombre
+        binding.txvDetailDescription.text = detalleAlumno.descripcion
+        binding.txvDetailHorario.text = detalleAlumno.horario
+        binding.txvP1.text = detalleAlumno.calif1.toString()
+        binding.txvP2.text = detalleAlumno.calif2.toString()
+        binding.txvP3.text = detalleAlumno.calif3.toString()
+        setImage(detalleAlumno.foto.toInt())
+
     }
 
+    private fun setImage(img: Int){
+        when(img){
+            1 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[0])
+            2 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[1])
+            3 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[2])
+            4 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[3])
+            5 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[4])
+            6 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[5])
+            7 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[6])
+            8 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[7])
+            9 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[8])
+            10 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[9])
+            11 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[8])
+            12 -> binding.imgDetailSubject.loadFromURLRounded(subjectPicture[9])
+        }
+    }
 }
